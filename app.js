07fa40dd98e853f2a1d494ec3d071e3bc119ab5c
Shashank Soni <shashanksoni815@gmail.com>
2024-11-13 02:58:55 +0530

    explore

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Listing = require("./models/listing.js")
 const path = require("path");
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override')
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/explore360";
 
@@ -25,6 +26,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(bodyParser.json());
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
+app.engine('ejs', ejsMate);
 
 
 app.get("/", (req, res) => {
