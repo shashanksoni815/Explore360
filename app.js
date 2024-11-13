3cbc232cb87d3400791eb0bf8ae68ed365934402
Shashank Soni <shashanksoni815@gmail.com>
2024-11-13 15:05:54 +0530

    explore

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.engine('ejs', ejsMate);
-
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
     res.send("Server is started");
