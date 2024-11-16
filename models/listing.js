849fa633ad62e969828a6d9ce6da499d4a0fea53
Shashank Soni <shashanksoni815@gmail.com>
2024-11-16 15:51:44 +0530

    Explore

--- models/listing.js
+++ models/listing.js
@@ -1,33 +1,12 @@
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
-
-// const listingSchema = new Schema({
-//   title: {
-//     type: String,
-//     required: true,
-//   },
-//   description: String,
-//   image: {
-//     type: String,
-//     required: true,
-
-//     default:
-//       "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
-//     set: (v) =>
-//       v === ""
-//         ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
-//         : v,
-//   },
-//   price: Number,
-//   location: String,
-//   country: String,
-// });
-
-// const Listing = mongoose.model("Listing", listingSchema);
-// module.exports = Listing;
-
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+
+// const Listing = require("./models/listing.js")
+
+
+// const imageElement = require("../init/data.js", {sampleListings.image.ur;})
+//   imageElement.src = imageData.url;
 
 const listingSchema = new Schema({
   title: {
@@ -36,9 +15,7 @@ const listingSchema = new Schema({
   },
   description: String,
   image: {
-    type: String,
-    // let src = url,
-    // url: src,
+    type: Object,
     default:
       "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
     set: (v) =>
