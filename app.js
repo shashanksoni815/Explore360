76cddd74ca9516d59ccabf9cb4dfe8c46ae26981
Shashank Soni <shashanksoni815@gmail.com>
2024-12-01 01:19:36 +0530

    review

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js")
 const ExpressError = require("./utils/ExpressError.js")
 const {listingSchema} = require("./schema.js")
-
+const Review = require("./models/review.js")
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/explore360";
 
@@ -99,6 +99,11 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
   console.log(deletedListing);
   res.redirect("/listings");
 }));
+
+// Reviews Post route
+app.post("/listings/:id/reviews", async(req, res) => {
+  let listing = await Listing.findById(req.params.id);
+})
 
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = new Listing({
