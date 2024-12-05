4ccf254d358530e193f48bd15569cc254d4fe559
Shashank Soni <shashanksoni815@gmail.com>
2024-12-06 02:21:19 +0530

    reviews Complete

--- models/listing.js
+++ models/listing.js
@@ -27,13 +27,20 @@ const listingSchema = new Schema({
   price: Number,
   location: String,
   country: String,
-  review: [
+  review : [
     {
       type: Schema.Types.ObjectId,
       ref: "Review",
     }
   ]
 });
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if(listing){
+  await review.deleteMany({_id : {$in: listing.reviews}});
+  }
+});
+
 
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
