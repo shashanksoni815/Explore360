1aa75aa3443581c027df049922d554d9f7f43286
Shashank Soni <shashanksoni815@gmail.com>
2024-12-07 23:48:55 +0530

    reviews Complete

--- models/review.js
+++ models/review.js
@@ -11,7 +11,7 @@ const reviewSchema = new Schema({
     createdAt: {
         type: Date,
         default: Date.now(),
-    }
+    },
 });
 
 module.exports = mongoose.model("Review", reviewSchema);
